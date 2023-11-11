edce90154e5e88df4ad17a05604c9c2d831e8ff2
Stefan M <StefMa@users.noreply.github.com>
2023-11-11 20:30:30 +0100

    Remove gradle wrapper upgrade plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,8 @@
 plugins {
     alias(libs.plugins.androidGradlePlugin) apply(false)
     alias(libs.plugins.kotlin) apply(false)
-    alias(libs.plugins.wrapperUpgrade)
 }
 
 subprojects {
     version = "2.2.2"
-}
-
-wrapperUpgrade {
-    gradle {
-        create("textref") {
-            repo.set("ioki-mobility/TextRef")
-            baseBranch.set("main")
-        }
-    }
 }
