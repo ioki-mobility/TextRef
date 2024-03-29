df2bedbca6b7817e220e3d73f0436ee288d64048
Stefan M <StefMa@users.noreply.github.com>
2024-03-29 10:22:02 +0100

    Update build.gradle.kts

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     signing
 }
 
-kotlinExtension.jvmToolchain(20)
+kotlinExtension.jvmToolchain(8)
 
 android {
     namespace = "com.ioki.textref"
@@ -18,8 +18,8 @@ android {
     }
 
     compileOptions { 
-         sourceCompatibility = JavaVersion.VERSION_1_17
-         targetCompatibility = JavaVersion.VERSION_1_17
+         sourceCompatibility = JavaVersion.VERSION_17
+         targetCompatibility = JavaVersion.VERSION_17
      } 
      kotlinOptions { jvmTarget = "17" }
 }
