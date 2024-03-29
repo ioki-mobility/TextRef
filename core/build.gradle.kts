d5e3672542fbb3243a6d11a08dad8849726d83c0
Stefan M <StefMa@users.noreply.github.com>
2024-03-29 09:47:35 +0100

    Try out jvm toolchains

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -16,11 +16,6 @@ android {
         minSdk = 21
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions { jvmTarget = "1.8" }
 }
 
 dependencies {
